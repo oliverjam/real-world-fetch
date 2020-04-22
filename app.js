786bddb04bc570a2b28f67dbbfabc3608c165d66
mesutBE <gelukkig.zijn.be@gmail.com>
2020-04-22 15:28:17 +0200

    task2 solution

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ class fetchInit {
         this.headers = { "content-type": "application/json" };
     }
 }
+
+document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault();
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    const data = { username, password };
+    const newInit = new fetchInit(data);
+
+    fetch("https://reqres.in/api/users", newInit)
+        .then(response => {
+            if (!response.ok) throw new Error(response.status);
+            return response.json();
+        })
+        .then(json => console.log(json))
+        .catch(error => console.error(error));
+});
--- init.js
+++ init.js
@@ -1,17 +1,3 @@
 window.onload = (event) => {
-    document.getElementById('start-fetch-button')
-        .onclick = (event) => {
-            const data = { name: "oli" };
-            const newInit = new fetchInit(data);
-
-            fetch(event.target.form.fetchUrl.value, newInit)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(response.status);
-                    }
-                    return response.json();
-                })
-                .then(json => console.log(json))
-                .catch(err => console.error('Erroooor: ' + err.message));
-        }
+    // 
 };
